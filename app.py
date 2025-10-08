c940f8cfa64352115ce11dfa3a452221bc42b759
Kevin Edin <kevin.edin@elev.ga.ntig.se>
2025-10-08 12:45:45 +0200

    lade till alive och dead funktioner

--- app.py
+++ app.py
@@ -6,19 +6,36 @@ class Enemy:
         self.health = hp
         self.damage = atk
         self.name = nam
+        self.alive = True
 
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+        
+    def ded(self):
+        if self.health <= 0:
+            self.alive = False
+            print(f"{self.name} dog.")
+        
     def print_status(self):
         print(f"Fiende med namnet {self.name} har {self.health} hp")
 
     def attack(self, target):
-        print(f"{self.name} attackerar dig!")
-        target = (target - self.damage)
-        return target
+        alive = self.is_alive()
+        if alive:
+            print(f"{self.name} attackerar {target.name}!")
+            target.health = (target.health - self.damage)
+            print(f"{target.name} har nu {target.health} hp")
+            target.ded()
     
     def take_damage(self, incoming_damage):
-        if self.health > 0:
+        alive = self.is_alive()
+        if alive:
             self.health = self.health - incoming_damage
             print(self.health)
+            self.ded()
 
 
 chicken = Enemy(3, 1, "Kyckling")
@@ -27,4 +44,6 @@ chicken.print_status()
 jesus = Enemy(999, 999, "Jesus")
 jesus.print_status()
 
-chicken.take_damage(3)
+chicken.attack(jesus)
+
+jesus.attack(chicken)
